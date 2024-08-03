2fee73f0ae2c0714b75abe1e9771ef33ec1f158d
Sudarshan Bhat <148414544+Sudarshan291107@users.noreply.github.com>
2024-08-03 22:04:15 +0530

    deleteuser.php

--- deleteuser.php
+++ deleteuser.php
@@ -3,7 +3,7 @@
 require_once('connection.php');
 $email=$_GET['id'];
 
-$sql="DELETE from users where EMAIL='$email'";
+$sql="DELETE from users where EMAIL='$email'"; 
 $result=mysqli_query($con,$sql);
 
 echo '<script>alert("USER DELETED SUCCESFULLY")</script>';
